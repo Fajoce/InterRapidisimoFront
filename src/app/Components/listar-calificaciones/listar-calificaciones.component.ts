9facd0f6cd4f9949e0e2e84a8cf67ff45e7f113c
fajoce <fajoce@gmail.com>
2025-05-19 07:44:30 -0500

    Agregue componente calificaciones

--- src/app/Components/listar-calificaciones/listar-calificaciones.component.ts
+++ src/app/Components/listar-calificaciones/listar-calificaciones.component.ts
@@ -2,53 +2,57 @@ import { Component, OnInit } from '@angular/core';
 import { CalificacionesService } from '../../Services/calificaciones.service';
 import { VerCalificacionesDTO, ActualizarCalificacionesDTO } from '../../Models/Calificaciones/CreateCalificacionDTO'
 import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+import { AuthLogService } from '../../Services/auth-log.service';
 
 @Component({
   selector: 'app-listar-calificaciones',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './listar-calificaciones.component.html',
   styleUrl: './listar-calificaciones.component.css'
 })
 export class ListarCalificacionesComponent implements OnInit {
   calificaciones: VerCalificacionesDTO[] = [];
-  calificacionSeleccionada: VerCalificacionesDTO | null = null;
   modoEditar: boolean = false;
+  esAdmin: any;
 
-  constructor(private calificacionesService: CalificacionesService) {}
+  constructor(private calificacionesService: CalificacionesService, 
+    private authService: AuthLogService,
+    public router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.cargarCalificaciones();
-  }
-
-  cargarCalificaciones(): void {
-    this.calificacionesService.getCalificaciones().subscribe(data => {
-      this.calificaciones = data;
-      console.log(data)
-    });
-  }
-
-  ver(calificacion: VerCalificacionesDTO): void {
-    this.calificacionSeleccionada = calificacion;
-    this.modoEditar = false;
-  }
-
-  editar(calificacion: VerCalificacionesDTO): void {
-    this.calificacionSeleccionada = { ...calificacion };
-    this.modoEditar = true;
-  }
-
-  eliminar(id: number): void {
-    if (confirm('¿Estás seguro de que quieres eliminar esta calificación?')) {
-      this.calificacionesService.deleteCalificacion(id).subscribe(() => {
-        this.cargarCalificaciones();
-        this.calificacionSeleccionada = null;
-        this.modoEditar = false;
+      this.esAdmin = this.authService.isAdmin();
+      this.obtenerCalificaciones();
+    
+    }
+  
+    obtenerCalificaciones(): void {
+      this.calificacionesService.getCalificaciones().subscribe({
+        next: (res) => this.calificaciones = res,
+        error: (err) => console.error('Error al obtener calificaciones', err)
       });
     }
+  
+    verDetalle(id: number): void {
+      this.router.navigate(['/calificaciones', id, 'detalle']);
+    }
+  
+    editar(id: number): void {
+      this.router.navigate(['/calificaciones', id, 'editar']);
+    }
+  
+    eliminar(id: number): void {
+       this.esAdmin = this.authService.isAdmin();
+      if (confirm('¿Estás seguro de eliminar esta calificacion?')) {
+        this.calificacionesService.deleteCalificacion(id).subscribe({
+          next: () => this.obtenerCalificaciones(),
+          error: (err) => console.error('Error al eliminar calificacion', err)
+        });
+      }
+    }
+    crearAlumno(): void {
+    this.router.navigate(['/alumnos/crear']);
   }
-
-  refrescar(): void {
-    this.modoEditar = false;
-    this.cargarCalificaciones();
   }
-}
+  
